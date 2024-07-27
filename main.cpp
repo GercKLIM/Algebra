7b9c0fa3a66382a07fbbab81397e27430666ed62
GercKLIM <gerceg0808@yandex.ru>
2024-07-27 12:40:26 +0300

    v.1.3

--- main.cpp
+++ main.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
 #include "Lib/algebra.h"
+//#include "Lib/vectors/TEST_vectors.cpp"
 
 int main() {
-
-    //std::vector<Vector<double>> E = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
-    std::vector<std::vector<double>> E = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
-    Matrix<double> m1(E);
-    std::cout << m1 - m1 << std::endl;
-
 
     std::cout << "Complete!" << std::endl;
     return 0;
